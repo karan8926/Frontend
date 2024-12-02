a7b312d0c0d4a4a253649c835fce0ae9cdf44614
gk12 <gk44035@gmail.com>
2024-12-02 13:31:31 +0530

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,14 +13,14 @@ const Navbar = () => {
   return (
     <div className="w-full h-[6rem]  bg-white p-6 shadow-slate-800 shadow-2xl flex justify-between">
       <div className="p-2 ">
-        <h3 className=" w-full p-2 rounded-xl text-center bg-yellow-400 text-yellow-700 font-bold text-lg">
+        <h3 className=" w-full p-2 rounded-xl text-center  text-yellow-700 font-bold text-lg">
           {userName}
         </h3>
       </div>
       <div className="flex w-[20%]">
         <button
           onClick={logout}
-          className=" w-[50%] rounded-lg bg-red-400 text-red-700 font-bold text-lg"
+          className=" w-[50%] rounded-lg bg-red-400 text-white font-bold text-lg"
         >
           Logout
         </button>

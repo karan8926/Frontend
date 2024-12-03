3ac868530c06d3d73ca0ad7db8a0fa20bc4ed1f6
gk12 <gk44035@gmail.com>
2024-12-03 15:24:50 +0530

    loader,calendar

---
+++ src/components/AvailabilityCalendar.jsx
@@ -0,0 +1,69 @@
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+const myEventsList = [
+  {
+    title: "Available Slot",
+    start: moment("2024-12-05T09:00:00").toDate(),
+    end: moment("2024-12-05T09:30:00").toDate(),
+    resource: "1",
+  },
+  {
+    title: "Available Slot",
+    start: moment("2024-12-05T10:00:00").toDate(),
+    end: moment("2024-12-06T10:30:00").toDate(),
+    resource: "2",
+  },
+  {
+    title: "Available Slot",
+    start: moment("2024-12-06T11:00:00").toDate(),
+    end: moment("2024-12-06T11:30:00").toDate(),
+    resource: "3",
+  },
+  {
+    title: "Available Slot",
+    start: moment("2024-12-07T14:00:00").toDate(),
+    end: moment("2024-12-07T14:30:00").toDate(),
+    resource: "1",
+  },
+];
+
+function onSelectEventFunction() {
+  console.log("i am available");
+}
+const AvailabilityCalendar = () => {
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 600 }}
+        views={["month", "week", "day", "agenda"]}
+        selectable={false}
+        onSelectEvent={() => onSelectEventFunction()}
+        eventPropGetter={(event) => ({
+          style: {
+            backgroundColor: "#4CAF50",
+            color: "white",
+            borderRadius: "5px",
+          },
+        })}
+      />
+
+      {/* <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      /> */}
+    </div>
+  );
+};
+
+export default AvailabilityCalendar;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { RotatingLines } from "react-loader-spinner";
+
+const Loader = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "50vh",
+      }}
+    >
+      <RotatingLines
+        visible={true}
+        height="96"
+        width="96"
+        color="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  );
+};
+
+export default Loader;

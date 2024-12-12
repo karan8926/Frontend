770c81188ee19136567a1542c21d1b65c9982e4c
gk12 <gk44035@gmail.com>
2024-12-12 16:16:27 +0530

    patient list page updated

--- src/pages/AdminDashboard/PatientList.jsx
+++ src/pages/AdminDashboard/PatientList.jsx
@@ -7,6 +7,7 @@ import { toast } from "react-toastify";
 import { baseUrl } from "../../App";
 import { useNavigate } from "react-router-dom";
 import { TbRefresh } from "react-icons/tb";
+import Loader from "../../components/Loader";
 const PatientList = () => {
   const [patientList, setPatientList] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
@@ -14,6 +15,9 @@ const PatientList = () => {
   const [toggleModel, setToggleModel] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [accessCode, setAccessCode] = useState("");
+  const [addPatientLoader, setAddPatientLoader] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPhoneValid, setIsPhoneValid] = useState(true);
   const [formData, setFormData] = useState({
     accessCode: "",
     name: "",
@@ -50,10 +54,16 @@ const PatientList = () => {
       ...formData,
       [name]: value,
     });
+    if (name === "email") {
+      validateEmail(value);
+    } else if (name === "phone_number") {
+      validatePhone(value);
+    }
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     // console.log(formData);
+    setAddPatientLoader(true);
     try {
       const { name, email, phone_number } = formData;
       const createPatient = await axios.post(`${baseUrl}api/patient-signup`, {
@@ -63,6 +73,8 @@ const PatientList = () => {
         accessCode,
       });
       setToggleModel(false);
+      setAddPatientLoader(false);
+      setAddPatientLoader;
       toast.success("Access code generated successfully");
     } catch (error) {
       toast.error(error.response.data.error, "failed to add");
@@ -95,6 +107,21 @@ const PatientList = () => {
   useEffect(() => {
     generateAccessCode();
   }, []);
+  // form Validation
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setIsEmailValid(emailRegex.test(email));
+  };
+
+  const validatePhone = (phone) => {
+    const phoneRegex = /^\d{10}$/; // Example: Validate 10 digits
+    setIsPhoneValid(phoneRegex.test(phone));
+  };
+
+  const isFormValid = () => {
+    // Form is valid if both email and phone are valid
+    return isEmailValid && isPhoneValid;
+  };
   return (
     <div className="w-full h-screen flex ">
       <Sidebar />
@@ -116,98 +143,114 @@ const PatientList = () => {
               </div>
               {toggleModel && (
                 <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
-                  <div className="w-[40%] max-w-lg bg-white p-6  rounded-md flex flex-col">
-                    <h2 className="text-xl font-semibold text-center mb-4">
-                      Add Patient
-                    </h2>
-                    <form onSubmit={handleSubmit} className="space-y-4">
-                      <div>
-                        <label
-                          htmlFor="name"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Access Code
-                        </label>
-                        <div className="flex items-center border border-gray-300 rounded-md w-full h-10 px-3 py-2">
-                          <h3 className="flex-grow">{accessCode}</h3>
-                          <TbRefresh
-                            className="text-gray-600 size-8 cursor-pointer"
-                            onClick={() => generateAccessCode()}
-                          />
-                        </div>
+                  {addPatientLoader ? (
+                    <Loader />
+                  ) : (
+                    <div className="w-[40%] max-w-lg bg-white p-6  rounded-md flex flex-col">
+                      <h2 className="text-xl font-semibold text-center mb-4">
+                        Add Patient
+                      </h2>
+                      <form onSubmit={handleSubmit} className="space-y-4">
+                        <div>
+                          <label
+                            htmlFor="name"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Access Code
+                          </label>
+                          <div className="flex items-center border border-gray-300 rounded-md w-full h-10 px-3 py-2">
+                            <h3 className="flex-grow">{accessCode}</h3>
+                            <TbRefresh
+                              className="text-gray-600 size-8 cursor-pointer"
+                              onClick={() => generateAccessCode()}
+                            />
+                          </div>
 
-                        {/* <button
+                          {/* <button
                           className="cursor-pointer bg-red-500 text-white"
                           onClick={generateAccessCode}
                         >
                           Generate Access-code
                         </button> */}
-                      </div>
-                      <div>
-                        <label
-                          htmlFor="name"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Name
-                        </label>
-                        <input
-                          type="text"
-                          id="name"
-                          name="name"
-                          value={formData.name}
-                          onChange={handleChange}
-                          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
-                        />
-                      </div>
-                      <div>
-                        <label
-                          htmlFor="email"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Email
-                        </label>
-                        <input
-                          type="email"
-                          id="email"
-                          name="email"
-                          value={formData.email}
-                          onChange={handleChange}
-                          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
-                        />
-                      </div>
-                      <div>
-                        <label
-                          htmlFor="phone_number"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Phone Number
-                        </label>
-                        <input
-                          type="tel"
-                          id="phone_number"
-                          name="phone_number"
-                          value={formData.phone_number}
-                          onChange={handleChange}
-                          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
-                        />
-                      </div>
-                      <div className="flex justify-end mt-4">
-                        <button
-                          type="submit"
-                          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-                        >
-                          Submit
-                        </button>
-                        <button
-                          type="button"
-                          onClick={() => setToggleModel(false)}
-                          className="ml-2 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700"
-                        >
-                          Close
-                        </button>
-                      </div>
-                    </form>
-                  </div>
+                        </div>
+                        <div>
+                          <label
+                            htmlFor="name"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Name
+                          </label>
+                          <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                            required
+                          />
+                        </div>
+                        <div>
+                          <label
+                            htmlFor="email"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Email
+                          </label>
+                          <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            className={`mt-1 block w-full px-3 py-2 border ${
+                              isEmailValid
+                                ? "border-gray-300"
+                                : "border-red-500"
+                            } rounded-md`}
+                            required
+                          />
+                        </div>
+                        <div>
+                          <label
+                            htmlFor="phone_number"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Phone Number
+                          </label>
+                          <input
+                            type="number"
+                            id="phone_number"
+                            name="phone_number"
+                            value={formData.phone_number}
+                            onChange={handleChange}
+                            className={`mt-1 block w-full px-3 py-2 border ${
+                              isPhoneValid
+                                ? "border-gray-300"
+                                : "border-red-500"
+                            } rounded-md`}
+                            required
+                          />
+                        </div>
+                        <div className="flex justify-end mt-4">
+                          <button
+                            type="submit"
+                            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+                            disabled={addPatientLoader || !isFormValid()}
+                          >
+                            Submit
+                          </button>
+                          <button
+                            type="button"
+                            onClick={() => setToggleModel(false)}
+                            className="ml-2 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700"
+                          >
+                            Close
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  )}
                 </div>
               )}
               <table className="w-full table-auto">

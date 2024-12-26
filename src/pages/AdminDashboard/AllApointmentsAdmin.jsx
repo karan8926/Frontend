18014c1371ce8e84d61c04a0929024f5dd99c9cb
gk12 <gk44035@gmail.com>
2024-12-26 19:02:25 +0530

    responsive design

--- src/pages/AdminDashboard/AllApointmentsAdmin.jsx
+++ src/pages/AdminDashboard/AllApointmentsAdmin.jsx
@@ -79,73 +79,89 @@ const AllApointmentsAdmin = () => {
     return endTimeFormatted;
   }
   return (
-    <div className="w-full h-screen flex ">
+    <div className="w-full h-screen flex  ">
       <Sidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col w-[85%]">
         <Navbar />
-        <div className="flex-1 bg-gray-100 p-6">
-          <div className="w-full overflow-hidden ">
+        <div className="flex-1 bg-gray-100 p-6 ">
+          <div className="w-full ">
             <div className="w-full h-[30%]  ">
               <div className="w-full h-8 ">
                 <h1 className="font-bold text-3xl">Appointments</h1>
               </div>
-              <table className="w-full table-auto">
-                <thead>
-                  <tr className="h-10">
-                    <th className="font-bold p-2 text-left">sr no</th>
-                    <th className="font-bold p-2 text-left">Patient Name</th>
-                    <th className="font-bold p-2 text-left">Patient Email</th>
-                    <th className="font-bold p-2 text-left">Therapist Name</th>
-                    <th className="font-bold p-2 text-left">Therapist Email</th>
-                    <th className="font-bold p-2 text-left">
-                      Therapist Specialty
-                    </th>
-                    <th className="font-bold p-2 text-left">
-                      Therapist Region
-                    </th>
-                    <th className="font-bold p-2 text-left">Date</th>
-                    <th className="font-bold p-2 text-left">Time Slot</th>
-                    <th className="font-bold p-2 text-left">Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {appointments?.map((data, index) => (
-                    <tr key={index} className="border-t">
-                      <td className="p-2">{index + 1}</td>
-                      <td className="p-2">{data?.appointment?.name}</td>
-                      <td className="p-2 break-words max-w-xs">
-                        {data?.appointment?.email}
-                      </td>
-                      <td className="p-2">{data.therapistDetails[0]?.name}</td>
-                      <td className="p-2 break-words max-w-xs">
-                        {data.therapistDetails[0]?.email}
-                      </td>
-                      <td className="p-2">
-                        {data.therapistDetails[0]?.specialty}
-                      </td>
-                      <td className="p-2">
-                        {data.therapistDetails[0]?.region}
-                      </td>
-                      {/* <td className="p-2">{data.name}</td> */}
-                      <td className="p-2">{DateTime(data.date)}</td>
-                      <td className="p-2">
-                        {data.time}-
-                        {timeSlotFunction(data.time, data.appointmentType)}
-                      </td>
-                      <td className="p-2">
-                        {/* Status Label with background color and fixed width */}
-                        <span
-                          className={`p-2 text-white rounded ${getStatusColor(
-                            data.status
-                          )} w-32 text-center inline-block`}
-                        >
-                          {data.status}
-                        </span>
-                      </td>
+              <div className="min-w-[64rem] w-full  overflow-x-scroll">
+                <table className="w-full  table-auto ">
+                  <thead>
+                    <tr className="h-10">
+                      <th className="font-bold p-2 text-left whitespace-nowrap  pr-4">
+                        sr no
+                      </th>
+                      <th className="font-bold p-2 text-left whitespace-nowrap pr-4">
+                        Patient Name
+                      </th>
+                      <th className="font-bold p-2 text-left whitespace-nowrap pr-4">
+                        Patient Email
+                      </th>
+                      <th className="font-bold p-2 text-left whitespace-nowrap pr-4">
+                        Therapist Name
+                      </th>
+                      <th className="font-bold p-2 text-left whitespace-nowrap pr-4">
+                        Therapist Email
+                      </th>
+                      <th className="font-bold p-2 text-left whitespace-nowrap pr-4">
+                        Therapist Specialty
+                      </th>
+                      <th className="font-bold p-2 text-left whitespace-nowrap pr-4">
+                        Therapist Region
+                      </th>
+                      <th className="font-bold p-2 text-left">Date</th>
+                      <th className="font-bold p-2 text-left whitespace-nowrap pr-4">
+                        Time Slot
+                      </th>
+                      <th className="font-bold p-2 text-left">Status</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {appointments?.map((data, index) => (
+                      <tr key={index} className="border-t">
+                        <td className="p-2 pr-4">{index + 1}</td>
+                        <td className="p-2 pr-4">{data?.appointment?.name}</td>
+                        <td className="p-2 pr-4 break-words max-w-xs">
+                          {data?.appointment?.email}
+                        </td>
+                        <td className="p-2 pr-4">
+                          {data.therapistDetails[0]?.name}
+                        </td>
+                        <td className="p-2 pr-4 break-words max-w-xs">
+                          {data.therapistDetails[0]?.email}
+                        </td>
+                        <td className="p-2 pr-4">
+                          {data.therapistDetails[0]?.specialty}
+                        </td>
+                        <td className="p-2 pr-4">
+                          {data.therapistDetails[0]?.region}
+                        </td>
+                        {/* <td className="p-2">{data.name}</td> */}
+                        <td className="p-2 pr-4">{DateTime(data.date)}</td>
+                        <td className="p-2 pr-4">
+                          {data.time}-
+                          {timeSlotFunction(data.time, data.appointmentType)}
+                        </td>
+                        <td className="p-2 pr-4">
+                          {/* Status Label with background color and fixed width */}
+                          <span
+                            className={`p-2 pr-4 text-white rounded ${getStatusColor(
+                              data.status
+                            )} w-32 text-center inline-block`}
+                          >
+                            {data.status}
+                          </span>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
 
               <Pagination
                 currentPage={currentPage}
